639b258ff685b2753f1c3ae0983697a0086fd20b
Sourdaci <sourdaci@gmail.com>
2015-02-23 21:21:32 +0100

    Responder genera selecciona respuestas aleatoriamente de su ArrayList definido

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Date;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -9,8 +12,25 @@ public class Responder{
     /**
      * Construct a Responder - nothing to do
      */
-    public Responder()
-    {
+    
+    private Random aleatorio;
+    private ArrayList<String> respuestas;
+    
+    public Responder(){
+        aleatorio = new Random(new Date().getTime());
+        respuestas = new ArrayList<String>();
+        asignaRespuestas();
+    }
+    
+    private void asignaRespuestas(){
+        respuestas.add("Eso parece un problema serio...");
+        respuestas.add("¿Has probado a apagar y volver a encender?");
+        respuestas.add("¿Le dejaste el ordenador a algun amigo chistoso?");
+        respuestas.add("Tiene mala pinta. Parece un fallo de hardware.");
+        respuestas.add("Yo no habria hecho eso: acabas de invalidar la garantia");
+        respuestas.add("Comprueba que tienes todo bien conectado");
+        respuestas.add("Los CD de Windows al reves estan muy sobrevalorados");
+        respuestas.add("Eres funcionario, ¿verdad?. Cafe en el teclado otra vez...");
     }
 
     /**
@@ -18,6 +38,6 @@ public class Responder{
      * @return   A string that should be displayed as the response
      */
     public String generateResponse(){
-        return "That sounds interesting. Tell me more...";
+        return respuestas.get(aleatorio.nextInt(respuestas.size()));
     }
 }
